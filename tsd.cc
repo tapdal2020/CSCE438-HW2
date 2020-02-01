b91a6f5ad8b834a4231a9adeb67fe0847670a424
luke grammer <luke.grammer@github.tamu.edu>
2020-02-01 11:42:40 -0600

    Added data persistence for tracked users

--- tsd.cc
+++ tsd.cc
@@ -22,6 +22,7 @@
 #include <vector>
 #include <algorithm>
 #include <regex>
+#include <fstream>
 
 #include <grpc++/grpc++.h>
 
@@ -37,7 +38,9 @@ class TSNServiceImpl final : public TSN::Service {
     Status AddUser(ServerContext* context, const AddUserRequest* request,
                    AddUserReply* reply) override;
     public:
-      std::vector<std::string> users;
+    	std::ifstream infile;
+    	std::ofstream outfile;
+      	std::vector<std::string> users;
 };
 
 Status TSNServiceImpl::AddUser(ServerContext* context, const AddUserRequest* request,
@@ -50,43 +53,66 @@ Status TSNServiceImpl::AddUser(ServerContext* context, const AddUserRequest* req
         reply->set_status(3);
         return Status::OK;
     }
+    // Make sure username is not taken
     else if (std::find(users.begin(), users.end(), request->username()) != users.end())
     {
         // Return username already exists
-	      reply->set_status(1);
+	    reply->set_status(1);
         return Status::OK;
     }
+    // Username is valid and available
     else
     {
+    	// Add username to list of users
         users.push_back(request->username());
-        // TODO: WRITE TO DATA FILE FOR PERSISTENCE
+        
+        // Append username to users file
+        outfile.open("data/users.txt", std::ios_base::app);
+        if (!outfile) {
+        	std::cout << "ERROR: Could not write to users file!\n";
+        	return Status::CANCELLED;
+       	}
+        outfile << request->username() << "\n";
+        outfile.close();
+        std::cout << "Registered new user " << request->username() << "\n";
+
         reply->set_status(0);
-	      return Status::OK;
+	    return Status::OK;
     }
 }
 
 void RunServer() {
-  std::string server_address("0.0.0.0:3010");
-  TSNServiceImpl service;
-  // TODO: READ EXISTING DATA FILES FOR PERSISTENCE
+  	std::string server_address("0.0.0.0:3010");
+  	TSNServiceImpl service;
+	service.infile.open("data/users.txt");
+	if (service.infile) {
+		std::cout << "Found existing users file, importing users..\n";
+		std::string user;
+		while(std::getline(service.infile, user)) {
+			std::cout << "Registering new user " << user << "\n";
+			service.users.push_back(user);
+		}
+		service.infile.close();
+	}	
+		
+  	ServerBuilder builder;
+  	// Listen on the given address without any authentication mechanism.
+  	builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  	// Register "service" as the instance through which we'll communicate with
+  	// clients. In this case it corresponds to an *synchronous* service.
+  	builder.RegisterService(&service);
+  	// Finally assemble the server.
+  	std::unique_ptr<Server> server(builder.BuildAndStart());
+  	std::cout << "Server listening on " << server_address << std::endl;
 
-  ServerBuilder builder;
-  // Listen on the given address without any authentication mechanism.
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  // Register "service" as the instance through which we'll communicate with
-  // clients. In this case it corresponds to an *synchronous* service.
-  builder.RegisterService(&service);
-  // Finally assemble the server.
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << "Server listening on " << server_address << std::endl;
-
-  // Wait for the server to shutdown. Note that some other thread must be
-  // responsible for shutting down the server for this call to ever return.
-  server->Wait();
+  	// Wait for the server to shutdown. Note that some other thread must be
+  	// responsible for shutting down the server for this call to ever return.
+  	server->Wait();
 }
 
 int main(int argc, char** argv) {
-  RunServer();
+  	RunServer();
 
-  return 0;
+  	return 0;
 }
+
